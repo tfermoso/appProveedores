56cddb4788e33bc109f8af924dcfbb69481df5b6
tfermoso <tomas.fermoso@gmail.com>
2024-01-16 16:43:42 +0100

    Creamos paquete com.ceica y modelo Proveedor

--- src/com/ceica/Main.java
+++ src/com/ceica/Main.java
@@ -1,3 +1,5 @@
+package com.ceica;
+
 public class Main {
     public static void main(String[] args) {
 
---
+++ src/com/ceica/Modelos/Proveedor.java
@@ -0,0 +1,78 @@
+package com.ceica.Modelos;
+
+public class Proveedor {
+    private int id;
+    private String cif;
+    private String nombre;
+    private String direccion;
+    private String localidad;
+    private String provincia;
+
+    public Proveedor() {
+    }
+
+    public Proveedor(String cif, String nombre) {
+        this.cif = cif;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public java.lang.String getCif() {
+        return cif;
+    }
+
+    public void setCif(java.lang.String cif) {
+        this.cif = cif;
+    }
+
+    public java.lang.String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(java.lang.String nombre) {
+        this.nombre = nombre;
+    }
+
+    public java.lang.String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(java.lang.String direccion) {
+        this.direccion = direccion;
+    }
+
+    public java.lang.String getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(java.lang.String localidad) {
+        this.localidad = localidad;
+    }
+
+    public java.lang.String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(java.lang.String provincia) {
+        this.provincia = provincia;
+    }
+
+    @Override
+    public String toString() {
+        return "Proveedor{" +
+                "id=" + id +
+                ", cif='" + cif + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", localidad='" + localidad + '\'' +
+                ", provincia='" + provincia + '\'' +
+                '}';
+    }
+}
