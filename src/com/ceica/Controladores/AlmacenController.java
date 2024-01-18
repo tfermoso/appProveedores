c5be2f8777bd15a93e1de24a767aa76320d23a57
tfermoso <tomas.fermoso@gmail.com>
2024-01-18 20:05:17 +0100

    Método para buscar pedidos por proveedor

--- src/com/ceica/Controladores/AlmacenController.java
+++ src/com/ceica/Controladores/AlmacenController.java
@@ -6,6 +6,9 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Clase que representa el controlador de un almacén.
+ */
 public class AlmacenController {
 
     private List<Proveedor> proveedorList;
@@ -26,6 +29,16 @@ public class AlmacenController {
 
     }
 
+    /**
+     * Método para añadir un nuevo proveedor al almacén.
+     *
+     * @param cif       El CIF del proveedor.
+     * @param nombre    El nombre del proveedor.
+     * @param direccion La dirección del proveedor.
+     * @param localidad La localidad del proveedor.
+     * @param provincia La provincia del proveedor.
+     * @return true si se añadió el proveedor con éxito, false de lo contrario.
+     */
     public boolean nuevoProveedor(String cif,String nombre,String direccion,String localidad,String provincia){
         Proveedor proveedor=new Proveedor(cif,nombre);
         proveedor.setDireccion(direccion);
@@ -50,6 +63,11 @@ public class AlmacenController {
         return proveedorList.removeIf(proveedor -> cif.equals(proveedor.getCif()));
     }
 
+    /**
+     * @param cif CIF del proveedor
+     * @param nombre Nuevo nombre para el proveedor
+     * @return True si puede editar el nombre del proveedor
+     */
     public boolean editarNombreProveedor(String cif, String nombre){
         /*
         for (int i = 0; i < proveedorList.size(); i++) {
@@ -79,6 +97,12 @@ public class AlmacenController {
                 .orElse(false);
 
     }
+
+    /**
+     * @param id idpieza
+     * @param precio nuevo precio
+     * @return true si puede editar el precio
+     */
     public boolean editarPrecioPieza(int id,Double precio){
         return piezaList.stream()
                 .filter(pieza -> pieza.getId()==id)
@@ -90,6 +114,13 @@ public class AlmacenController {
                 .orElse(false);
     }
 
+    /**
+     * @param nombre Nombre de la pieza
+     * @param color Color de la pieza
+     * @param precio Precio de la pieza
+     * @param idcategoria idCategoria de la categoría de la pieza
+     * @return true si puede crear la pieza
+     */
     public boolean nuevaPieza(String nombre, Color color, Double precio, int idcategoria){
             Pieza pieza=new Pieza(nombre,color.toString(),precio);
             pieza.setCategoria(getCategoriaById(idcategoria));
@@ -97,12 +128,23 @@ public class AlmacenController {
             return true;
     }
 
+    /**
+     * @param id Id de la categoria
+     * @return Categoria del id solicitado
+     */
     private Categoria getCategoriaById(int id){
         return categorias.stream()
                 .filter(c->c.getId()==id).
                 findFirst().get();
     }
 
+    /**
+     * Método para grabar nuevo pedido. El pedido se graba con la fecha actual
+     * @param cif CIF del proveedor
+     * @param idPieza ID de la pieza
+     * @param cantidad Cantidad solicitada
+     * @return Información sobre si ha completado el pedido o no
+     */
     public String nuevoPedido(String cif,int idPieza,int cantidad){
         Proveedor proveedor=getProveedorByCIF(cif);
         if(proveedor!=null){
@@ -120,6 +162,11 @@ public class AlmacenController {
             return "Error al crear el pedido, Proveedor no existe";
         }
     }
+
+    /**
+     * @param id ID de la pieza
+     * @return Pieza buscada por ID
+     */
     private Pieza getPiezaByID(int id){
         for (int i = 0; i < piezaList.size(); i++) {
             if(piezaList.get(i).getId()==id){
@@ -128,6 +175,11 @@ public class AlmacenController {
         }
         return null;
     }
+
+    /**
+     * @param cif CIF del proveedor buscado
+     * @return Proveedor buscado
+     */
     private Proveedor getProveedorByCIF(String cif){
         for (Proveedor p:proveedorList){
             if(cif.equals(p.getCif())){
@@ -137,6 +189,10 @@ public class AlmacenController {
         return null;
     }
 
+    /**
+     * @param idPieza ID de la Pieza
+     * @return String Lista de pedidos de la pieza solicitada en formato string
+     */
     public String getPedidosByPieza(int idPieza){
         List<Pedido> pedidosByPieza=new ArrayList<>();
         for (Pedido pedido : pedidoList){
@@ -150,15 +206,28 @@ public class AlmacenController {
             return "No hay pedidos de esta pieza";
         }
     }
+
+    /**
+     * Buscar proveedor por CIF
+     * @param cif CIF del proveedor
+     * @return String Lista de pedidos del proveedor
+     */
     public String getPedidosByProveedor(String cif){
-        return "";
+        return pedidoList.stream()
+                .filter(pedido -> cif.equals(pedido.getProveedor().getCif()))
+                .toList()
+                .stream()
+                .findFirst()
+                .map(Object::toString)
+                .orElse("No hay pedidos de ese proveedor");
     }
     @Override
     public String toString() {
         return "AlmacenController{" + "\n"+
                 "proveedorList=" + proveedorList + "\n"+
                 ", piezaList=" + piezaList + "\n"+
-                ", pedidoList=" + pedidoList + "\n"+
+                ", pedidoList="
+                + pedidoList + "\n"+
                 '}';
     }
 
--- src/com/ceica/Main.java
+++ src/com/ceica/Main.java
@@ -10,11 +10,9 @@ public class Main {
         almacen.nuevoProveedor("a","nombre","dir","loc","pro");
         almacen.nuevaPieza("piezaA", Color.AZUL,45.5,1);
         almacen.nuevaPieza("piezaB", Color.NEGRO,5.5,2);
-        System.out.println(almacen.toString());
-        almacen.editarPrecioPieza(1,65.4);
         System.out.println(almacen.nuevoPedido("a",1,50));
         System.out.println(almacen.nuevoPedido("a",1,10));
-        System.out.println(almacen.toString());
-
+        System.out.println(almacen.nuevoPedido("j",0,30));
+        System.out.println(almacen.getPedidosByProveedor("a"));
     }
 }
